97570dac0845469c945e1f070bb0b32544313a03
atillman <atillman@a0e3710e-d0f0-0f07-7f71-f519f5f91ac9>
2009-07-15 19:56:10 +0000

    Initial project entry

---
+++ trunk/metadata.rb
@@ -0,0 +1,83 @@
+require 'yaml'
+require 'rets_helper'
+
+include RetsHelper
+
+def dump_system(metadata)
+  system = metadata.system
+  puts "System ID: " + system.system_id
+  puts "Description: " + system.system_description
+  puts "Comment: " + system.comments
+end
+
+# standard_name
+
+def dump_all_resources(metadata)
+  puts "Resources: "
+  metadata.GetAllResources.each do |resource|
+    standard_name = resource.standard_name
+    visible_name = resource.GetStringAttribute("VisibleName", standard_name)
+    puts " - #{resource.resource_id} (#{standard_name}, #{visible_name})"
+  end
+end
+
+def dump_all_classes(metadata, resource)
+  resource_name = resource.resource_id;
+  puts "Classes for #{resource_name}:"  
+  metadata.GetAllClasses(resource_name).each() do |aClass|
+    visible_name = aClass.GetStringAttribute("VisibleName", "")
+    puts " - #{aClass.class_name} (#{aClass.standard_name}, #{visible_name})"
+  end
+end
+
+def dump_all_tables(metadata, resource, aClass)
+  puts "Tables for #{resource.resource_id}-#{aClass.class_name}:"
+  metadata.all_tables(aClass).each do |table|
+    short_name = table.GetStringAttribute("ShortName", "")
+    long_name = table.GetStringAttribute("LongName", "")
+    required =  table.GetStringAttribute("Required", "")
+    puts "- #{table.system_name} (#{table.standard_name}, #{short_name} (#{long_name})) - Required: #{required}"
+  end
+end
+
+def dump_all_lookups(metadata, resource)
+  resource_name = resource.resource_id();
+  lookups = metadata.all_lookups(resource_name).collect(){|lookup| "#{lookup.lookup_name}(#{lookup.visible_name})"}
+  lookups = lookups.join("\n - ")
+  puts "Lookups for #{resource_name}: \n - #{lookups}"
+end
+
+def dump_all_lookup_types(metadata, resource, lookup)
+  puts "Lookup Types for #{resource.resource_id}-#{lookup.lookup_name}(#{lookup.visible_name}):"
+  puts
+  metadata.all_lookup_types(lookup).each do |lookup_type|
+    puts "Lookup value: " + lookup_type.value + " (" +
+      lookup_type.short_value + ", " +
+      lookup_type.long_value + ")"
+  end
+end
+
+config = YAML.load_file(ARGV[0])
+execute_metadata_action(config[:rets_info]) do |metadata|
+  puts
+  dump_system(metadata)
+  puts
+  if(ARGV.length > 2)
+    resource = metadata.GetResource(ARGV[1])
+    if(ARGV[2].upcase == "LOOKUPS")
+      if(ARGV.length == 4)
+        dump_all_lookup_types(metadata, resource, metadata.GetLookup(resource.resource_id, ARGV[3]))
+      else
+        dump_all_lookups(metadata, resource)      
+      end
+    else
+      dump_all_tables(metadata, resource, metadata.getClass(resource.resource_id, ARGV[2]))
+    end
+  elsif(ARGV.length == 2)
+    dump_all_classes(metadata, metadata.GetResource(ARGV[1]))
+  else
+    dump_all_resources(metadata)
+  end
+  puts
+  
+end
---
+++ trunk/rets_helper.rb
@@ -0,0 +1,41 @@
+require 'rubygems'
+require 'librets'
+
+include Librets
+
+module RetsHelper 
+  
+  def execute_rets_action(rets_info)
+    session = RetsSession.new(rets_info[:url])
+    if(rets_info.has_key?(:user_agent))
+      session.user_agent = rets_info[:user_agent]
+    end
+    if(rets_info.has_key?(:log_file))
+      session.http_log_name = rets_info[:log_file]
+    end
+    if(session.login(rets_info[:username], rets_info[:password]))
+      begin
+        yield(session)
+      ensure
+        session.logout
+      end
+    else
+      raise RetsLoginError.new("Error logging into RETS Server", rets_info[:url])
+    end
+  end
+  
+  def execute_metadata_action(rets_info)
+    execute_rets_action(rets_info) do |session|
+      yield(session.metadata)
+    end
+  end
+  
+  class RetsLoginError < Exception
+    attr_reader :session_context
+    
+    def initialize(message, session_context)
+      super(message)
+      @session_context = session_context
+    end
+  end
+end
