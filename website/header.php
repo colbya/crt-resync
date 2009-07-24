7d4169c728cc8e3f54107a3e7920f481e870f2a1
atillman <atillman@a0e3710e-d0f0-0f07-7f71-f519f5f91ac9>
2009-07-24 21:38:43 +0000

    Added the website

---
+++ website/header.php
@@ -0,0 +1,91 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<!-- <?php
+
+# 
+function setup_globals()
+{
+  $self = $_SERVER["PHP_SELF"];
+  $script = realpath($_SERVER["SCRIPT_FILENAME"]);
+  $file = realpath(__FILE__);
+
+#  echo "self: $self\n";
+#  echo "script: $script\n";
+#  echo "file: $file\n";
+
+  $doc_root =  ereg_replace(dirname($self) . "$", "", dirname($script));
+  $GLOBALS["DOCUMENT_ROOT"] = $doc_root;
+  $GLOBALS["ROOT"] = str_replace($doc_root, "", dirname($file));
+}
+
+setup_globals();
+function ROOT()
+{
+    echo $GLOBALS["ROOT"];
+}
+
+
+$nav_urls = array("Home" => "/",
+                  "Documentation" => "/documentation/",
+                  "Downloads" => "/downloads/",
+                  "Support" => "/support/");
+
+function nav($url, $name)
+{
+    echo "<a href=\"" . $GLOBALS["ROOT"] . $url . "\"\>" . $name . "</a>";
+}
+
+function enav($name)
+{
+    $url = $GLOBALS["ROOT"] . $GLOBALS["nav_urls"][$name];
+    if ($GLOBALS["tmpl_location"] == $name) {
+        echo $name ;
+    }
+    else {
+        echo "<a href=\"" . $url . "\">" . $name . "</a>";
+    }
+}
+
+function ver()
+{
+    return "1.0";
+}
+
+function print_ver()
+{
+    echo ver();
+}
+
+function beta_ver()
+{     
+   # return "3.0.0b4";
+    return null;
+}     
+      
+function print_beta_ver()
+{
+    echo beta_ver();
+}
+
+?> -->
+<html>
+  <head>
+    <title><?php echo "simpleRETS: A Simple Command Line RETS Client"; ?></title>
+    <link href="<?php ROOT() ?>/main.css" type="text/css"
+          rel="STYLESHEET">
+    </style>
+  </head>
+
+  <body bgcolor="#ffffff">
+    <table width="100%"class="header">
+        <tr>
+          <td class="hleft">simpleRETS</td>
+          <td class="hright">
+        <?php enav("Home") ?> |
+        <?php enav("Documentation") ?> |
+        <?php enav("Downloads") ?> |
+        <?php enav("Support") ?>
+          </td>
+        </tr>
+    </table>
+
+    <div class="content">
---
+++ website/support/index.php
@@ -0,0 +1,13 @@
+<?php $tmpl_title = "Support";
+      $tmpl_location = "Support";
+      include("../header.php");
+?>
+    <h3>Support</h3>
+    <p>
+       For more information about the Center for
+       REALTOR<sup>&reg;</sup> Technology or simpleRETS, please send email
+       to <a
+       href="mailto:info@crt.realtors.org">info@crt.realtors.org</a>
+    </p>
+
+    <?php include("../footer.php"); ?>
