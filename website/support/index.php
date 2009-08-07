70ac2430d5466d9f1918356ee25546dc1e694241
atillman <atillman@a0e3710e-d0f0-0f07-7f71-f519f5f91ac9>
2009-08-07 18:51:11 +0000

    Add link to mailinglist.

--- website/support/index.php
+++ website/support/index.php
@@ -3,6 +3,11 @@
       include("../header.php");
 ?>
     <h3>Support</h3>
+	<p>
+	      Please join the <a
+	      href="http://mail.crt.realtors.org/mailman/listinfo/simplerets">simplerets
+	      mailing list</a>, and send questions there.
+	    </p>
     <p>
        For more information about the Center for
        REALTOR<sup>&reg;</sup> Technology or simpleRETS, please send email
