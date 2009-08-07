a372b683904b628930f5e2966b644833b190b99f
atillman <atillman@a0e3710e-d0f0-0f07-7f71-f519f5f91ac9>
2009-08-07 18:54:16 +0000

    Added support for user agent password and defining the RETS version.

--- trunk/rets_helper.rb
+++ trunk/rets_helper.rb
@@ -17,7 +17,15 @@ module RetsHelper
     session = RetsSession.new(rets_info[:url])
     if(rets_info.has_key?(:user_agent))
       session.user_agent = rets_info[:user_agent]
+      if(rets_info.has_key?(:user_agent_password))
+        session.user_agent_password = rets_info[:user_agent_password]
+      end      
     end
+    
+    if(rets_info.has_key?(:rets_version))
+      session.rets_version = create_rets_version(rets_info[:rets_version])
+    end
+    
     if(rets_info.has_key?(:log_file))
       session.http_log_name = rets_info[:log_file]
     end
@@ -35,6 +43,18 @@ module RetsHelper
   def execute_metadata_action(rets_info)
     execute_rets_action(rets_info) do |session|
       yield(session.metadata)
+    end
+  end
+  
+  def create_rets_version(version) 
+    if version.to_s == "1.7.2" then
+      return Librets::RETS_1_7_2 
+    elsif version.to_s == "1.7" then
+      return Librets::RETS_1_7 
+    elsif version.to_s == "1.5" then
+      return Librets::RETS_1_5
+    else
+      raise "Unknown RETS version #{version}"
     end
   end
   
