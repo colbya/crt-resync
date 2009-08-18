a4f573ceaada45f1ad6651b8161f9a0e6cd3e500
atillman <atillman@a0e3710e-d0f0-0f07-7f71-f519f5f91ac9>
2009-08-18 20:45:17 +0000

    Moved some steps along.

--- object_fetch.rb
+++ object_fetch.rb
@@ -6,22 +6,32 @@ require 'rets_helper'
 include RetsHelper
 
 config = YAML.load_file(ARGV[0])
+object_type = ARGV[1]
+
+content_type_extension = {"image/jpeg" => "jpg"}
 
 execute_rets_action(config[:rets_info]) do |session|    
-  get_object_request = GetObjectRequest.new("Property", "Photo")
+  get_object_request = GetObjectRequest.new(config[:resource], object_type)
   get_object_request.add_all_objects("1")
+  get_object_request.add_all_objects("2")
+  get_object_request.add_all_objects("3")
+  get_object_request.add_all_objects("4")
+  get_object_request.add_all_objects("5")
   get_object_response = session.get_object(get_object_request)
-  get_object_response.each_object do |object_descriptor|
-  	object_key =  object_descriptor.object_key
-  	object_id = object_descriptor.object_id
-  	content_type = object_descriptor.content_type
-  	description = object_descriptor.description
-  	print "#{object_key} object \##{object_id}"
-  	print ", description: #{description}" if !description.empty?
-  	puts
-  	  
-  	Zip::ZipFile.open("test.zip", Zip::ZipFile::CREATE) do |zipfile|    
-      output_file_name = object_id.to_s + ".jpg"
+  Zip::ZipFile.open("test.zip", Zip::ZipFile::CREATE) do |zipfile|    
+    get_object_response.each_object do |object_descriptor|
+  	  object_key =  object_descriptor.object_key
+  	  object_id = object_descriptor.object_id
+  	  content_type = object_descriptor.content_type
+  	  description = object_descriptor.description
+  	  print "#{object_key} object \##{object_id}"
+  	  print ", description: #{description}" if !description.empty?
+  	  puts
+  	  extension = content_type_extension[content_type]
+  	  if(extension != nil) then
+  	    extension = "." + extension
+	    end
+      output_file_name = object_id.to_s + extension
       zipfile.file.open(object_key + "/" + output_file_name, "w") do |f|
         f << object_descriptor.data_as_string
       end
