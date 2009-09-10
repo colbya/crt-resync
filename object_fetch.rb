39336e1d5d3208d02623340a9ce3f1fa5ed83f56
atillman <atillman@a0e3710e-d0f0-0f07-7f71-f519f5f91ac9>
2009-09-10 20:12:40 +0000

    Added progressbar.  Need to just the requests.

--- object_fetch.rb
+++ object_fetch.rb
@@ -3,6 +3,7 @@ require 'csv'
 require 'rets_helper'
 require 'zip/zipfilesystem'
 require 'rets_helper'
+require 'progressbar'
 
 include RetsHelper
 
@@ -25,33 +26,36 @@ id_index = headers.index(config[:item_id])
 ids = data_file.collect{|row| row.at(id_index)}
 
 execute_rets_action(config[:rets_info]) do |session|    
-  config[:object_types].each do |object_type|
-    get_object_request = GetObjectRequest.new(config[:resource], object_type)
-    ids.each{|id| get_object_request.add_all_objects(id)}
-    STDERR.puts "Getting #{object_type}"
-    get_object_response = session.get_object(get_object_request)
+  config[:object_types].each do |object_type|        
     STDERR.puts "Zipping #{object_type} to #{object_type}.zip"
     Zip::ZipFile.open("#{object_type}.zip", Zip::ZipFile::CREATE) do |zipfile|    
-      get_object_response.each_object do |object_descriptor|
-  	    object_key =  object_descriptor.object_key
-  	    object_id = object_descriptor.object_id
-  	    content_type = object_descriptor.content_type
-  	    description = object_descriptor.description
-  	    STDERR.puts "Adding #{object_key} #{object_type} \##{object_id}"
-  	    STDERR.puts ", description: #{description}" if !description.empty?
-  	    puts
-  	    extension = content_type_extension[content_type]
-  	    if(extension != nil) then
-  	      extension = "." + extension
-	      end
-        output_file_name = object_id.to_s + extension
-        zipfile.file.open(object_key + "/" + output_file_name, "w") do |f|
-          f << object_descriptor.data_as_string
+      pbar = ProgressBar.new("Progress", ids.length)
+      ids.each do |id|
+        get_object_request = GetObjectRequest.new(config[:resource], object_type)
+        get_object_request.add_all_objects(id)
+        get_object_response = session.get_object(get_object_request)
+        get_object_response.each_object do |object_descriptor|
+          object_key =  object_descriptor.object_key
+          object_id = object_descriptor.object_id
+          content_type = object_descriptor.content_type
+          description = object_descriptor.description
+          extension = content_type_extension[content_type]
+          if(extension != nil) then
+            extension = "." + extension
+          end
+          output_file_name = object_id.to_s + extension
+          zipfile.file.open(object_key + "/" + output_file_name, "w") do |f|
+            f << object_descriptor.data_as_string
+          end
         end
+        pbar.inc      
       end  	
+      STDERR.puts
     end
+    STDERR.puts "Done zipping #{object_type}.zip"
   end
 end
+STDERR.puts "DONE!"
 
 
 
