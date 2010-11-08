0e5ecc200f73da68b00f28da75c2610974284616
andrewtillmancrt <andrewtillmancrt@a0e3710e-d0f0-0f07-7f71-f519f5f91ac9>
2010-11-08 01:29:30 +0000

    Issue 3.  Added ability to use arbitrary number of parameters.  This breaks the old date parameters as well as the automatic last date feature

--- rets_data_fetch.rb
+++ rets_data_fetch.rb
@@ -21,81 +21,105 @@ require 'progressbar'
 
 include RetsHelper
 
-config = YAML.load_file(ARGV[0])
-
-def insert_dates_into_dmql(dmql, dates)
-  if(dates.length > 1)
-    sart_month = Date.parse(dates[0])
-    first_of_start_month = Date.new(sart_month.year, sart_month.month, 1)
-    end_month = Date.parse(dates[1])
-    end_month = Date.new(end_month.year, end_month.month,1)
-    last_of_end_month = (end_month>>1)-1
-    dmql.gsub("<<start>>", first_of_start_month.to_s).gsub("<<end>>", last_of_end_month.to_s)
-  else
-    date = ARGV.length > 1 ? Date.parse(dates[0]) : Date.today<<1
-    first_of_month = Date.new(date.year, date.month, 1)
-    last_of_month = (first_of_month>>1)-1
-    dmql.gsub("<<start>>", first_of_month.to_s).gsub("<<end>>", last_of_month.to_s)
+def parse_options(args)
+  if args.length < 1
+    return nil
+  elsif args.member?("--version")
+    return {:version => true}
+  else 
+    #Get the config file   
+    config = {:config_file => args.shift}    
+    
+    #Parse the added -D parameters
+    args.each do |arg| 
+      if param = arg.match('(.+)=(.+)')
+        config[param.captures[0]] = param.captures[1]
+      else
+        return nil
+      end
+    end
+    return config  
   end
 end
 
-dmql = insert_dates_into_dmql(config[:dmql], ARGV[1..2])
+def run_fetch(options)
 
-execute_rets_action(config[:rets_info]) do |session|    
-  request = session.create_search_request(config[:resource], config[:class], dmql)
-  request.standard_names = false
-  limit = -1
-  if(config[:limit]) 
-    limit = request.limit = config[:limit]    
+  #Get the config file.
+  unless FileTest.exists?(options[:config_file])
+    abort("Config File '#{options[:config_file]}' not found.")
   end
+  config = YAML.load_file(options[:config_file])
 
-  mappings = {}
-  if(config[:select]) 
-    selects = []
-    config[:select].each do |field|
-      if(field.class==Hash)
-        field.each do |field_name, mapping|
-          mappings[field_name] = mapping
-          selects.push(field_name)
+  dmql = config[:dmql]
+  options.each {|name, value| dmql = dmql.gsub("<<#{name}>>", value)}
+
+  execute_rets_action(config[:rets_info]) do |session|    
+    request = session.create_search_request(config[:resource], config[:class], dmql)
+    request.standard_names = false
+    limit = -1
+    if(config[:limit]) 
+      limit = request.limit = config[:limit]    
+    end
+
+    mappings = {}
+    if(config[:select]) 
+      selects = []
+      config[:select].each do |field|
+        if(field.class==Hash)
+          field.each do |field_name, mapping|
+            mappings[field_name] = mapping
+            selects.push(field_name)
+          end
+        else
+          selects.push(field)
         end
-      else
-        selects.push(field)
       end
+      request.set_select selects.join(",")
     end
-    request.set_select selects.join(",")
-  end
-  response = session.search(request)
-  
-  requested = (limit > -1 and limit < response.count) ? limit : response.count
-  
-  #Count the actual rows returned
-  count = 0
-    
-  #Create a progress bar for sanity on large downloads
-  version = get_version
-  libVersion = get_lib_version
-  STDERR.puts get_product_information("RETS Data Fetcher")
-  STDERR.puts "Fetching #{requested} of #{response.count} items"
-  pbar = ProgressBar.new("Progress", requested)
-  
-  #Write the results out to STDOUT
-  columns = response.columns
-  mapped_columns = columns.collect{|column| mappings.fetch(column, column)}
+    response = session.search(request)
 
-  CSV::Writer.generate(STDOUT) do |csv|
-    csv << mapped_columns
-    while(count < requested)
-      response.each do |result|    
-        csv << columns.collect {|column| result.string(column)}
-        pbar.inc
-        count = count + 1
-      end        
-      if(count < requested)
-        request.offset=count
-        response = session.search(request)
+    requested = (limit > -1 and limit < response.count) ? limit : response.count
+
+    #Count the actual rows returned
+    count = 0
+
+    #Create a progress bar for sanity on large downloads  
+    STDERR.puts "Fetching #{requested} of #{response.count} items"
+    pbar = ProgressBar.new("Progress", requested)
+
+    #Write the results out to STDOUT
+    columns = response.columns
+    mapped_columns = columns.collect{|column| mappings.fetch(column, column)}
+
+    CSV::Writer.generate(STDOUT) do |csv|
+      csv << mapped_columns
+      while(count < requested)
+        response.each do |result|    
+          csv << columns.collect {|column| result.string(column)}
+          pbar.inc
+          count = count + 1
+        end        
+        if(count < requested)
+          request.offset=count
+          response = session.search(request)
+        end
       end
     end
+    STDERR.puts ""
+    STDERR.puts "Done!"
   end
-  STDERR.puts ""
-  STDERR.puts "Done!"
+end
+
+#The script itself
+unless options = parse_options(ARGV)
+  abort("usage:  rets_data_fetch --version\n" + 
+        "        rets_data_fetch config_file [name1=value1 name2=value2 ...]")  
+end
+
+version = get_version
+libVersion = get_lib_version
+STDERR.puts get_product_information("RETS Data Fetcher")
+
+unless options[:version]
+  run_fetch(options)
 end
